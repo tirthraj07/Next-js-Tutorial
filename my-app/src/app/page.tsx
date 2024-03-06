e3abbe65e08085f639736f6be6441be98ded8ec6
Tirthraj Mahajan <39341897+tirthraj07@users.noreply.github.com>
2024-03-07 01:56:14 +0530

    Adding Tailwind CSS to Next JS

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,3 +1,19 @@
+import Link from "next/link";
 export default function Home(){
-    return <h1>Home Page</h1>
+    return (
+        <>
+    
+            <h1>Home Page</h1>
+        
+            <div>
+                <h3>Links</h3>
+                <Link href="/about">About</Link>
+                <br />
+                <Link href="/profile">Profile</Link>     
+                <br />
+                <Link href="/products">Products</Link>
+            </div>
+
+        </>)
+    ;
 }
