94a8044c86091ab13e4c7016e4798fd9f190c272
Tirthraj Mahajan <39341897+tirthraj07@users.noreply.github.com>
2024-03-07 01:02:30 +0530

    Next.js 14 Tutorial - 17 - Routing Metadata

--- my-app/src/app/products/[productId]/page.tsx
+++ my-app/src/app/products/[productId]/page.tsx
@@ -1,8 +1,35 @@
 import { notFound } from "next/navigation";
+import { Metadata } from "next";
 
-export default function productDetails( { params } : {
-    params : {productId: string}
-} ){
+type Props = {
+    params:{
+        productId:string
+    };
+};
+
+
+export const generateMetadata = ( {params}:(Props) ): Metadata =>{
+    return {
+        title:`Product ${params.productId}`
+    }
+};
+
+
+
+// Simulation of API Call
+// export const generateMetadata = async({params}:Props): Promise<Metadata> =>{
+//     const title = await new Promise((resolve)=>{
+//         setTimeout(()=>{
+//             resolve(`Product ${params.productId}`)
+//         },100)
+//     })
+//     return{
+//         title: `${title}`
+//     }
+// }
+
+
+export default function productDetails( { params } : Props ){
 
     if( parseInt(params.productId) > 10){
         notFound();
