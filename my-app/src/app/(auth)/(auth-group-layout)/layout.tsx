bb28291a13078912ec3e8f2f078ddf088b7fc3d5
Tirthraj Mahajan <39341897+tirthraj07@users.noreply.github.com>
2024-03-07 00:17:15 +0530

    Next.js 14 Tutorial - 16 - Route Group Layout

---
+++ my-app/src/app/(auth)/(auth-group-layout)/layout.tsx
@@ -0,0 +1,16 @@
+export default function authLayout({children}:{
+    children:React.ReactNode
+}){
+    return(
+    <>
+        {children}
+
+        <div>
+            <p>This is a Route Group Layout</p>
+            <p>This is only available for /login and /signup</p>
+            <p>This is not seen in /forgot-password</p>
+            <p>This is achieved by using <strong>Route Groups</strong> studied in lesson 13</p>
+        </div>
+    </>
+    );
+}
