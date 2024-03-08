e9ac4393d1c791c143095b5882ebd5244da3e0ed
Tirthraj Mahajan <39341897+tirthraj07@users.noreply.github.com>
2024-03-08 20:28:58 +0530

    Next.js 14 Tutorial - 23 - Loading UI

--- my-app/src/app/blog/page.tsx
+++ my-app/src/app/blog/page.tsx
@@ -1,9 +1,19 @@
-import { Metadata } from "next";
+// import { Metadata } from "next";
+import { resolve } from "path";
 
-export const metadata:Metadata = {
-    title: {
-        absolute: "Blog"
-    }
+// export const metadata:Metadata = {
+//     title: {
+//         absolute: "Blog"
+//     }
+// }
+
+export const generateMetadata = async() =>{
+    const title = await new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve('Blog')
+        },100)
+    })
+
 }
 
 export default function Blog(){
---
+++ my-app/src/app/loading.tsx
@@ -0,0 +1,10 @@
+export default function Loading(){
+    return(
+        <>
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 border border-black min-w-75 text-center p-5">
+                <h1>Loading...</h1>
+            </div>
+
+        </>
+    )
+}
