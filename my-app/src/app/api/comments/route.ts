b11ef713cf2edd331d35a65fe26678f599be1e4d
Tirthraj Mahajan <39341897+tirthraj07@users.noreply.github.com>
2024-03-12 01:36:27 +0530

    Installing UUID for future

--- my-app/src/app/api/comments/route.ts
+++ my-app/src/app/api/comments/route.ts
@@ -7,7 +7,7 @@ export async function GET() {
 export async function POST(request:Request){
     const data = await request.json();
     const newComment = {
-        id : comments.length + 1,
+        id : comments.length+1,
         text: data.text
     }
     comments.push(newComment);
