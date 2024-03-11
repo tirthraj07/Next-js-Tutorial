d0ebd3df7c41dac0c939dd2221cf937abcaf3145
Tirthraj Mahajan <39341897+tirthraj07@users.noreply.github.com>
2024-03-12 00:47:28 +0530

    Next.js 14 Tutorial - 35 - Handling POST Request

--- my-app/src/app/api/comments/route.ts
+++ my-app/src/app/api/comments/route.ts
@@ -3,3 +3,19 @@ import { comments } from "./comments"
 export async function GET() {
     return Response.json(comments);
 }
+
+export async function POST(request:Request){
+    const data = await request.json();
+    const newComment = {
+        id : comments.length + 1,
+        text: data.text
+    }
+    comments.push(newComment);
+    return new Response(JSON.stringify(newComment),{
+        headers:{
+            "Content-Type":"application/json"
+        },
+        status:201
+    })
+
+}
