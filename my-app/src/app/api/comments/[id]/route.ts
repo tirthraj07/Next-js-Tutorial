fb714f874dd6c933c903cb521b491b7bd1839dd0
Tirthraj Mahajan <39341897+tirthraj07@users.noreply.github.com>
2024-03-12 02:07:47 +0530

    Next.js 14 Tutorial - 37 - Handling PATCH Request

--- my-app/src/app/api/comments/[id]/route.ts
+++ my-app/src/app/api/comments/[id]/route.ts
@@ -1,3 +1,4 @@
+import { comment } from "postcss";
 import { comments } from "../comments";
 export async function GET(request:Request, { params }:{
     params:{
@@ -22,4 +23,38 @@ export async function GET(request:Request, { params }:{
         },
         status: 404
     });
+}
+
+
+export async function PATCH(request:Request, { params }:{
+    params:{
+        id:string
+    }
+}){
+    const body = await request.json();
+    const { text } = body;
+
+
+    const index = comments.findIndex(
+        comment => comment.id === parseInt(params.id)
+    );
+
+    if(index === -1){
+        return new Response(JSON.stringify({"error":"Comment Not Found!"}),{
+            headers:{
+                "Content-Type":"application/json"
+            },
+            status:404
+        })
+    }
+
+    comments[index].text = text;
+    return new Response(JSON.stringify(comments[index]), {
+        headers:{
+            "Content-Type":"application/json"
+        },
+        status:200
+    })
+
+
 }
--- my-app/src/app/api/comments/comments.ts
+++ my-app/src/app/api/comments/comments.ts
@@ -5,10 +5,10 @@ export const comments = [
     },
     {
         id:2,
-        text:"This is a first comment"
+        text:"This is a second comment"
     },
     {
         id:3,
-        text:"This is a first comment"
+        text:"This is a third comment"
     }
 ];
